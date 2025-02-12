ac83a276dd1501e8f08c1321f7709fabe4fd9559
RogerioMatos75 <rogerio.matos75@hotmail.com>
2025-02-12 19:00:09 -0300

    Sua mensagem de commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ import java.util.Properties
 import java.io.FileInputStream
 
 plugins {
-    id("com.android.application") version "8.1.0"
+    id("com.android.application") version "8.2.0"
     id("org.jetbrains.kotlin.android") version "1.8.10"
     id("com.google.gms.google-services") version "4.3.15"
 }
